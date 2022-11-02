4ed1bc01c68efb8691fd399908394d4586d90580
PSC Student <psc@billkuker.com>
2022-11-02 11:06:21 -0400

    I did it

--- personal-dictionary.js
+++ personal-dictionary.js
@@ -1,1 +1,14 @@
-//Fill in this empty file
+//aray of words
+let words = [];
+
+//search function
+//paramter and returns true if it is found, false
+function search(word){
+
+}
+//addWord function
+function addWord(word){
+
+}
+//export
+export{search, addWord};
