9230f0d93383285886e633ce4b3213aa0e18380b
PSC Student <psc@billkuker.com>
2022-11-02 11:15:49 -0400

    done as well

--- personal-dictionary.js
+++ personal-dictionary.js
@@ -4,10 +4,11 @@ let words = [];
 //search function
 //paramter and returns true if it is found, false
 function search(word){
-
+    return words.includes(word);
 }
 //addWord function
 function addWord(word){
+    words.push(word)
 
 }
 //export
